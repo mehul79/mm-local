a7c586c2ed280cec413512796444c8b42f8a3a36
mehul79 <mehulrgupta@gmail.com>
2025-06-13 05:46:15 +0530

    comm

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -4,6 +4,8 @@ env_cfg = dict(
     mp_cfg=dict(mp_start_method='fork',        # Multi-processing method: 'fork' is default on Unix
                 opencv_num_threads=0),         # Disable OpenCV's internal threading to avoid conflict
     dist_cfg=dict(backend='nccl'),             # Sets the backend for distributed training to 'nccl' (GPU-friendly)
+                                               # to let gpu communicate efficiently with each other
+
 )
 
 vis_backends = [dict(type='LocalVisBackend')]
@@ -15,3 +17,10 @@ load_from = None
 resume = False
 
 tta_model = dict(type='SegTTAModel')
+
+'''
+  TTA is a technique where:
+  The input image is changed slightly (like flipped or resized).
+  The model makes predictions on each version.
+  Then all those predictions are combined (averaged) to get a final, better prediction.
+'''
