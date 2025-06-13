de3216b25eed20da0c5d9eed486def82f2d58c2f
mehul79 <mehulrgupta@gmail.com>
2025-06-13 05:35:02 +0530

    added comments

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,9 +1,11 @@
 default_scope = 'mmseg'
 env_cfg = dict(
-    cudnn_benchmark=True,
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
-    dist_cfg=dict(backend='nccl'),
+    cudnn_benchmark=True,                      # Enables CUDNN auto-tuner to find the best algorithm
+    mp_cfg=dict(mp_start_method='fork',        # Multi-processing method: 'fork' is default on Unix
+                opencv_num_threads=0),         # Disable OpenCV's internal threading to avoid conflict
+    dist_cfg=dict(backend='nccl'),             # Sets the backend for distributed training to 'nccl' (GPU-friendly)
 )
+
 vis_backends = [dict(type='LocalVisBackend')]
 visualizer = dict(
     type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer')
